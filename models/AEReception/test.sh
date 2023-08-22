c2ce9688ff607fd1980fe4dc39931bff1aea8ad1
Levi Starrett <levi@roxsoftware.com>
2023-08-22 11:11:49 -0400

    Add test setup for reception

---
+++ models/AEReception/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+AUDIT_EVENT_SCHEMA_PATH=schema/audit_event_schema.json
+RECEPTION_CONFIG_SCHEMA_PATH=schema/reception_config_schema.json
+CMD="AEReception_transient_standalone -log-level Debug -configPath config/ -postinit schedule/test.sch -util Inspector"
+mkdir -p test_results/
+DOCKER_RUN="docker compose -f ../../bin/docker-compose.yml run -e AUDIT_EVENT_SCHEMA_PATH=${AUDIT_EVENT_SCHEMA_PATH} -e RECEPTION_CONFIG_SCHEMA_PATH=${RECEPTION_CONFIG_SCHEMA_PATH} -v /${PWD}:/work run"
+${DOCKER_RUN} bash -c "source /work/build/Release/generators/conanrun.sh && export PATH=/work/build/Release/bin:${PATH} && ${CMD}"
