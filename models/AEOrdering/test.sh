ede3a3de610788c4ac2e0312a8852d8b67a30c87
Levi Starrett <levi@roxsoftware.com>
2023-08-22 11:48:24 -0400

    Add test scripts for other models

---
+++ models/AEOrdering/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+JOB_DEFINITION_SCHEMA_PATH=schema/job_definition_schema.json
+ORDERING_CONFIG_SCHEMA_PATH=schema/ordering_config_schema.json
+CMD="AEOrdering_transient_standalone -log-level Debug -configPath config/ -configFile aeordering_spec_test.json -postinit schedule/test.sch -startJobGroup 00 -endJobGroup FF -util Inspector"
+mkdir -p test_results/
+mkdir -p JobIdStore/
+DOCKER_RUN="docker compose -f ../../bin/docker-compose.yml run -e JOB_DEFINITION_SCHEMA_PATH=${JOB_DEFINITION_SCHEMA_PATH} -e ORDERING_CONFIG_SCHEMA_PATH=${ORDERING_CONFIG_SCHEMA_PATH} -v /${PWD}:/work run"
+${DOCKER_RUN} bash -c "source /work/build/Release/generators/conanrun.sh && export PATH=/work/build/Release/bin:${PATH} && ${CMD}"
---
+++ models/InvariantStore/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+CMD="IStore_transient_standalone -postinit schedule/test.sch -util Inspector"
+mkdir -p test_results/
+mkdir -p InvariantStore/
+DOCKER_RUN="docker compose -f ../../bin/docker-compose.yml run -v /${PWD}:/work run"
+${DOCKER_RUN} bash -c "source /work/build/Release/generators/conanrun.sh && export PATH=/work/build/Release/bin:${PATH} && ${CMD}"
