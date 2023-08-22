ae572ad990095a5bcacef3bdef59afc8a0612be5
Levi Starrett <levi@roxsoftware.com>
2023-08-22 15:03:08 -0400

    Update all tests to run without requiring directories to be created

--- models/SequenceVerificationDataCentric/test.sh
+++ models/SequenceVerificationDataCentric/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 CMD="AESequenceDC_transient_standalone -log-level Debug -postinit schedule/test.sch -util Inspector"
-mkdir -p test_results/
 DOCKER_RUN="docker compose -f ../../bin/docker-compose.yml run -v /${PWD}:/work run"
 ${DOCKER_RUN} bash -c "source /work/build/Release/generators/conanrun.sh && export PATH=/work/build/Release/bin:${PATH} && ${CMD}"
