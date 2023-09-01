3ad734950050455970f632e6b496fd4924bce5bc
Cortland Starrett <cort@roxsoftware.com>
2023-09-01 13:57:03 -0400

    job:  #MUN2-122  main merged build updated
    This brings Verification Gateway into the fold.

--- models/build_all.sh
+++ models/build_all.sh
@@ -5,6 +5,7 @@ bash -c "cd AEReception && ../../bin/build.sh $@"
 bash -c "cd FileReception && ../../bin/build.sh $@"
 bash -c "cd InvariantStore && ../../bin/build.sh $@"
 bash -c "cd SequenceVerificationDataCentric && ../../bin/build.sh $@"
+bash -c "cd VerificationGateway && ../../bin/build.sh $@"
 if [[ $# < 1 || $1 != "test" ]]; then
 	bash -c "cd PV_PROC && ../../bin/deploy.sh $@"
 fi
