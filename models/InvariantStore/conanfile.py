283020949db5893328a61234192c915e9fd6ca9c
Levi Starrett <levi@roxsoftware.com>
2023-09-22 12:47:24 -0400

    Bump version

--- models/InvariantStore/conanfile.py
+++ models/InvariantStore/conanfile.py
@@ -3,18 +3,18 @@ import conan
 
 class ConanFile(conan.ConanFile):
     name = 'istore'
-    version = '0.1'
+    version = '1.1.0'
     user = 'xtuml'
     channel = 'stable'
-    python_requires = 'masl_conan/[>=0.1]@xtuml/stable'
+    python_requires = 'masl_conan/[>=4.1.1]@xtuml/stable'
     python_requires_extend = 'masl_conan.MaslConanHelper'
 
     def masl_extras(self):
         return ['-skiptranslator', 'Sqlite']
 
     def requirements(self):
-        self.requires('masl_core/[>=0.1]@xtuml/stable')
-        self.requires('masl_utils/[>=0.1]@xtuml/stable')
+        self.requires('masl_core/[>=4.1.1]@xtuml/stable')
+        self.requires('masl_utils/[>=4.1.1]@xtuml/stable')
 
     def build_requirements(self):
-        self.tool_requires('masl_codegen/[>=0.1]@xtuml/stable')
+        self.tool_requires('masl_codegen/[>=4.1.1]@xtuml/stable')
