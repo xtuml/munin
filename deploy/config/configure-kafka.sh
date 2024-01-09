3a2c1387aaac7ed6b627aaef226d86144541bb53
Cortland Starrett <cort@roxsoftware.com>
2024-01-09 16:06:05 -0500

    docker local logging
    put back some AsyncLogger topics.
    Make run benchmark check for macos (darwin).

--- deploy/config/configure-kafka.sh
+++ deploy/config/configure-kafka.sh
@@ -2,6 +2,11 @@
 
 # configure topics
 /opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AsyncLogger_service0 --add-config retention.ms=5000
+/opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AsyncLogger_service1 --add-config retention.ms=5000
+/opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AsyncLogger_service2 --add-config retention.ms=5000
+/opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AsyncLogger_service3 --add-config retention.ms=5000
+/opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AsyncLogger_service4 --add-config retention.ms=5000
+/opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AsyncLogger_service5 --add-config retention.ms=5000
 /opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AEReception_service2 --add-config retention.ms=60000
 /opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AEOrdering_service2 --add-config retention.ms=300000
 /opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic default.AESequenceDC_service31 --add-config retention.ms=300000
