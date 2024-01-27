edb57a36d4c22c1ec93811c78993ffe5d1a2e1bd
Cortland Starrett <cort@roxsoftware.com>
2024-01-27 10:46:35 -0500

    job:  #MUN2-147  remove AER from build scripts

--- models/build_all.sh
+++ models/build_all.sh
@@ -3,7 +3,6 @@ set -e
 bash -c "cd AsyncLogger && ../../bin/build.sh $@"
 bash -c "cd BenchmarkingProbe && ../../bin/build.sh $@"
 bash -c "cd AEOrdering && ../../bin/build.sh $@"
-bash -c "cd AEReception && ../../bin/build.sh $@"
 bash -c "cd FileReception && ../../bin/build.sh $@"
 bash -c "cd InvariantStore && ../../bin/build.sh $@"
 bash -c "cd SequenceVerificationDataCentric && ../../bin/build.sh $@"
--- models/clean_all.sh
+++ models/clean_all.sh
@@ -3,7 +3,6 @@ set -e
 bash -c "cd AsyncLogger && ../../bin/clean.sh $@"
 bash -c "cd BenchmarkingProbe && ../../bin/clean.sh $@"
 bash -c "cd AEOrdering && ../../bin/clean.sh $@"
-bash -c "cd AEReception && ../../bin/clean.sh $@"
 bash -c "cd FileReception && ../../bin/clean.sh $@"
 bash -c "cd InvariantStore && ../../bin/clean.sh $@"
 bash -c "cd SequenceVerificationDataCentric && ../../bin/clean.sh $@"
