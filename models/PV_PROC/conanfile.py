631286aed432d033b67d6e8b81be5b0de98a4670
Levi Starrett <levi@roxsoftware.com>
2024-03-01 09:49:45 -0500

    export resource to conan packages

--- models/PV_PROC/conanfile.py
+++ models/PV_PROC/conanfile.py
@@ -8,6 +8,8 @@ class ConanFile(conan.ConanFile):
     python_requires = f'masl_conan/{os.environ["MASL_VERSION"]}@xtuml/stable'
     python_requires_extend = 'masl_conan.MaslConanHelper'
 
+    exports_sources = 'src/*', 'masl/*', 'schedule/*', 'config/*', 'schema/*'
+
     def requirements(self):
         self.requires(f'masl_core/{os.environ["MASL_VERSION"]}@xtuml/stable')
         self.requires(f'masl_utils/{os.environ["MASL_VERSION"]}@xtuml/stable')
