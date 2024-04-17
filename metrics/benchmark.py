023c28e3a74cdc186b4a46ca73b04a7f948f9b2b
Cortland Starrett <cort@roxsoftware.com>
2024-04-17 10:23:16 -0400

    job:  #188  benchmark probe working

--- metrics/benchmark.py
+++ metrics/benchmark.py
@@ -1,63 +1,16 @@
 import argparse
 import datetime
 import kafka3
-import re
+import json
 
-
-KEY_EVENTS = ('jobmanagement_event_received', 'jobmanagement_event_written', 'aeordering_events_processed', 'svdc_event_received', 'svdc_event_processed', 'svdc_happy_event_processed', 'svdc_unhappy_event_processed')
-PATTERN = r'EventId = ([\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12})'
-
-
-def decode_data(data, datatypes):
-    values = []
-    for datatype in datatypes:
-
-        if datatype == 'string':
-
-            # check to make sure there is a length at the beginning of the field
-            if len(data) < 4:
-                raise ValueError
-
-            # get the length
-            length = int.from_bytes(data[:4], byteorder='big')
-            data = data[4:]
-
-            # check to make sure the data contains 'length' more bytes
-            if len(data) < length:
-                raise ValueError
-
-            # extract the value
-            values.append(data[:length].decode('utf-8'))
-            data = data[length:]
-
-        elif datatype == 'timestamp':
-
-            # check to make sure there is enough remaining data
-            if len(data) < 8:
-                raise ValueError
-
-            nanos = int.from_bytes(data[:8], byteorder='big')
-            data = data[8:]
-
-            seconds = nanos / 1e9
-            values.append(datetime.datetime.fromtimestamp(seconds))
-
-        else:
-            raise ValueError
-
-    return tuple(values)
-
+KEY_EVENTS = ('jobmanagement_event_received', 'aeordering_event_processed', 'svdc_event_processed', 'svdc_happy_event_processed', 'svdc_unhappy_event_processed')
 
 class Event:
 
     def __init__(self, id):
         self.id = id
-        self.valid = True
         self.received = None
-        self.validated = None
-        self.written = None
         self.ordering_received = None
-        self.svdc_received = None
         self.processed = None
 
 
@@ -77,15 +30,16 @@ if __name__ == '__main__':
         for k, partition in raw_msgs.items():
             for msg in partition:
                 data = bytearray(msg.value)
-                label, event_content, d = decode_data(data, ('string', 'string', 'timestamp'))
+                try:
+                    j = json.loads(data.decode('utf-8'))
+                except json.decoder.JSONDecodeError:
+                    print(f'INVALID JSON')
+                    sys.exit(1)
+                else:
+                    label = j['tag']
+                    d = datetime.datetime.strptime(j['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
+                    evt_id = j['EventId']
                 if label in KEY_EVENTS:
-
-                    # extract the UUID from the event content
-                    match = re.search(PATTERN, event_content)
-                    if match:
-                        evt_id = match.group(1)
-                    else:
-                        continue
 
                     # create or find event
                     if evt_id not in events:
@@ -96,12 +50,8 @@ if __name__ == '__main__':
 
                     if label == 'jobmanagement_event_received':
                         evt.received = d
-                    elif label == 'jobmanagement_event_written':
-                        evt.written = d
-                    elif label == 'aeordering_events_processed':
+                    elif label == 'aeordering_event_processed':
                         evt.ordering_received = d
-                    elif label == 'svdc_event_received':
-                        evt.svdc_received = d
                     elif label in ('svdc_event_processed', 'svdc_happy_event_processed', 'svdc_unhappy_event_processed'):
                         evt.processed = d
 
@@ -110,9 +60,7 @@ if __name__ == '__main__':
     evts = events.values()
     print(f'Total number of events: {len(evts)}')
 #    print(f'Total number of events (Received by JobManagement): {len(list(filter(lambda e: e.received is not None, evts)))}')
-#    print(f'Total number of events (Written by JobManagement): {len(list(filter(lambda e: e.written is not None, evts)))}')
     print(f'Total number of events (Loaded by Ordering): {len(list(filter(lambda e: e.ordering_received is not None, evts)))}')
-    print(f'Total number of events (Received by SVDC): {len(list(filter(lambda e: e.svdc_received is not None, evts)))}')
     print(f'Total number of events (Processed by SVDC): {len(list(filter(lambda e: e.processed is not None, evts)))}')
 
     evts = list(filter(lambda e: e.ordering_received is not None and e.processed is not None, evts))
@@ -121,6 +69,4 @@ if __name__ == '__main__':
         td = max(map(lambda e: e.processed or datetime.min, evts)) - min(map(lambda e: e.ordering_received or datetime.max, evts))
         print(f'Total time: {td.total_seconds():.3f}')
         print(f'Events per second: {len(evts) / td.total_seconds():.3f}')
-#        print(f'Average time in reception per event: {sum(map(lambda e: (e.written - e.received).total_seconds(), evts)) / len(evts):.3f}s')
-#        print(f'Average time between reception and PV per event: {sum(map(lambda e: (e.ordering_received - e.written).total_seconds(), evts)) / len(evts):.3f}s')
         print(f'Average time in PV per event: {sum(map(lambda e: (e.processed - e.ordering_received).total_seconds(), evts)) / len(evts):.3f}s')
