4e04d77ca7d20e514383729d4cf4e77f00821390
Cortland Starrett <cort@roxsoftware.com>
2024-04-20 17:08:26 -0400

    job:  #188  logging to files and topics
    also adding pvstats.py which is exploring a dashboard

--- deploy/config/configure-kafka.sh
+++ deploy/config/configure-kafka.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # configure topics
-/opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic JobManagement_service0 --add-config retention.ms=60000
+/opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic Protocol_Verifier_Reception --add-config retention.ms=60000
+/opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic Protocol_Verifier_InfoWarn --add-config retention.ms=6000000
 /opt/kafka/bin/kafka-configs.sh --alter --bootstrap-server kafka:9093 --topic BenchmarkingProbe_service0 --add-config retention.ms=6000000
