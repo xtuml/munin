83eafa7e6fb8475a675a90aab6e74ccf34634be7
Cortland Starrett <cort@roxsoftware.com>
2024-05-21 13:40:18 -0400

    job:  #213  deleting AEReception and AsyncLogger
    The AEReception model and all artefacts are now deprecated and deleted.
    The AsyncLogger model and all artefacts are now deprecated and deleted.
    References in documentation have been updated.
    The docker compose file for production needed updates for AsyncLogger,
    but it also had not been modified for the changes in Kafka topics.  This
    has now been done in this branch.

--- models/AsyncLogger/masl/AsyncLogger/custom/custom.cmake
+++
@@ -1,5 +0,0 @@
-target_sources( AsyncLogger PRIVATE ${CMAKE_CURRENT_LIST_DIR}/AsyncLogger_services.cc )
-if ( NOT Logger_FOUND )
-find_package ( Logger CONFIG QUIET )
-endif()
-target_link_libraries( AsyncLogger PRIVATE Logger::Logger )
