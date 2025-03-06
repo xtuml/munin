f6dc49b4dd7a27a14c981b8258f8b30a8b4d031c
Levi Starrett <levi@roxsoftware.com>
2025-03-06 09:16:10 -0500

    Update conanfiles to point to new architecture dependencies

--- models/BenchmarkingProbe/conanfile.py
+++ models/BenchmarkingProbe/conanfile.py
@@ -6,13 +6,7 @@ class ConanFile(conan.ConanFile):
     name = 'benchmarkingprobe'
     user = 'xtuml'
     channel = 'stable'
-    python_requires = f'masl_conan/{os.environ["MASL_VERSION"]}@xtuml/stable'
-    python_requires_extend = 'masl_conan.MaslConanHelper'
+    python_requires = 'xtuml_masl_conan/[>=1.0 <2]@xtuml'
+    python_requires_extend = 'xtuml_masl_conan.MaslConanHelper'
 
     exports_sources = 'src/*', 'masl/*', 'schedule/*', 'config/*', 'schema/*'
-
-    def requirements(self):
-        self.requires(f'masl_core/{os.environ["MASL_VERSION"]}@xtuml/stable')
-
-    def build_requirements(self):
-        self.tool_requires(f'masl_codegen/{os.environ["MASL_VERSION"]}@xtuml/stable')
--- models/PV_PROC/conanfile.py
+++ models/PV_PROC/conanfile.py
@@ -5,20 +5,18 @@ class ConanFile(conan.ConanFile):
     name = 'pv_proc'
     user = 'xtuml'
     channel = 'stable'
-    python_requires = f'masl_conan/{os.environ["MASL_VERSION"]}@xtuml/stable'
-    python_requires_extend = 'masl_conan.MaslConanHelper'
+    python_requires = 'xtuml_masl_conan/[>=1.0 <2]@xtuml'
+    python_requires_extend = 'xtuml_masl_conan.MaslConanHelper'
 
     exports_sources = 'src/*', 'masl/*', 'schedule/*', 'config/*', 'schema/*'
 
     def requirements(self):
-        self.requires(f'masl_core/{os.environ["MASL_VERSION"]}@xtuml/stable')
-        self.requires(f'masl_utils/{os.environ["MASL_VERSION"]}@xtuml/stable')
         self.requires(f'aeordering/{self.version}@xtuml/stable')
         self.requires(f'freception/{self.version}@xtuml/stable')
         self.requires(f'istore/{self.version}@xtuml/stable')
         self.requires(f'aesequencedc/{self.version}@xtuml/stable')
         self.requires(f'verificationgateway/{self.version}@xtuml/stable')
         self.requires(f'jobmanagement/{self.version}@xtuml/stable')
-
-    def build_requirements(self):
-        self.tool_requires(f'masl_codegen/{os.environ["MASL_VERSION"]}@xtuml/stable')
+        self.requires('masl_json/[>=1.0 <2]@xtuml')
+        self.requires('masl_logger/[>=1.0 <2]@xtuml')
+        self.requires('masl_uuid/[>=1.0 <2]@xtuml')
