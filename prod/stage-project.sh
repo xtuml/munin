632a604f5ec90e7ae4c231e2db3e6284be9b84bd
Levi Starrett <levi@roxsoftware.com>
2025-04-08 10:29:08 -0400

    Add lockfiles to ensure the same versions are used

--- prod/stage-project.sh
+++ prod/stage-project.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v conan-cache:/conan-cache -v ${PWD}:/work levistarrett/masl-dev:latest conan install . --deployer-folder=staging --deployer-package=* --version=${MUNIN_VERSION}
+docker run --rm -v conan-cache:/conan-cache -v ${PWD}:/work levistarrett/masl-dev:latest conan install . --deployer-folder=staging --deployer-package=* --version=${MUNIN_VERSION} --lockfile-partial
